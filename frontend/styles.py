33f0aaff79feb9ac9fd0135488f00c7d46f63244
Ghost-of-Poseidon <geraldggmwangi@gmail.com>
2025-03-19 20:21:09 +0300

    Added styles for calculator UI

---
+++ frontend/styles.py
@@ -0,0 +1,15 @@
+# frontend/styles.py
+
+button_config = {
+    "width": 10,
+    "height": 2,
+    "font": ("Arial", 15),
+    "bg": "lightgray"
+}
+
+entry_config = {
+    "font": ("Arial", 20),
+    "bd": 10,
+    "relief": "ridge",
+    "justify": "right"
+}
