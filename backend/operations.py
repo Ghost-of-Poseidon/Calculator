8e8a9793ef57809f5e3e524bf13cc7fbdc39fa04
Ghost-of-Poseidon <geraldggmwangi@gmail.com>
2025-03-19 20:20:49 +0300

    Added backend initialization file

---
+++ backend/operations.py
@@ -0,0 +1,13 @@
+# backend/operations.py
+
+import math
+
+def power(base, exponent):
+    return base ** exponent
+
+def modulus(a, b):
+    return a % b
+
+def square_root(num):
+    return math.sqrt(num)
+
